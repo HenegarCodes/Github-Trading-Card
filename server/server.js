5772087f366fa3b7bd13ad6bf91c9aac6151a7d4
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2025-03-23 16:57:22 -0700

    finlaizing init

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 import express from 'express'
-
+import dotenv from "dotenv"
+import mongoose from 'mongoose'
 
 dotenv.config()
 
 const PORT = process.env.PORT
 
 const app = express();
+
+app.get("/",(req, res)=>
+{
+    res.send("API is running")
+})
 
 app.listen(PORT, ()=>
 console.log(`server is running on port ${PORT}`)
